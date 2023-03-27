8cf24f38257b2a8a9018570ecff7b6597eeb3186
SergeyTrutenko <60572935+SergeyTrutenko@users.noreply.github.com>
2023-03-27 19:23:51 +0300

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -18,3 +18,32 @@ $(".calculate-modal").on("hidden.bs.modal", function () {
     $(".calculate-content-wrap #step-2").hide();
     $(".calculate-content-wrap #step-3").hide();
 });
+
+$(".calculate-content-wrap input[name='type']").on("change", function (e){
+    if($(this).hasClass("js-radio-common")){
+        $(".calculate-content-wrap input.js-check-inv").prop("disabled", true)
+        $(".calculate-content-wrap input.js-check-inv").prop("checked",false)
+    }
+    else {
+        $(".calculate-content-wrap input.js-check-inv").prop("disabled", false)
+    }
+
+});
+
+$(document).on("change", ".calculate-content-wrap #step-3", function() {
+    var names = {};
+    $('.calculate-content-wrap #step-3 :radio').each(function() {
+        names[$(this).attr('name')] = true;
+    });
+
+    var count = 0;
+    $.each(names, function() {
+        count++;
+    });
+
+    if ($('.calculate-content-wrap #step-3 :radio:checked').length === count) {
+        $(".calculate-content-wrap .form-btn").prop("disabled", !this.checkValidity())
+    }
+});
+
+
